a447d3d3d678dd497b6e4d1988bedd095f57e8df
MalakRosas <malakrosass@gmail.com>
2024-04-10 04:10:18 +0200

    fixing

--- signUpBack.php
+++ signUpBack.php
@@ -3,7 +3,6 @@ session_start(); // Start the session to access session variables
 
 require_once 'connection.php'; // Include the database connection file
 require_once 'phpfunctions.php'; // Include the phpfunctions.php file
-
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["signup"])) {
     $username = $_POST['username'];
     $email = $_POST['email'];
@@ -11,23 +10,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["signup"])) {
 
     // Check if email is already used
     if (emailUsed($conn, $email)) {
-        $_SESSION["signupError"] = "Email is already in use. Please use a different email.";
-        header("location:index.html"); // Changed location to sign.html
+        // Email is already in use, set error message in session
+        echo   '<script>
+            alert("Email already exists.")
+            window.location.href = "signup.html";
+        </script>';
         exit();
     }
-
     // Attempt to create the user
     if (createUser($conn, $username, $email, $password)) {
         // Redirect to appropriate page after successful sign-up
-        header("location:sign.html");
+        header("location:signin.html");
         exit();
     } else {
         $_SESSION["signupError"] = "Registration failed. Please try again.";
-        header("location:sign.html"); // Changed location to sign.html
+        header("location:signup.html");
         exit();
     }
 } else {
-    header("location:sign.html"); // Changed location to sign.html
+    header("location:signup.html");
     exit();
 }
+
 ?>
