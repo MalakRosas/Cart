4cb352c821896b60514aafcdd898110c97b9cddb
MalakRosas <malakrosass@gmail.com>
2024-04-18 22:45:56 +0200

    products

--- signInBack.php
+++ signInBack.php
@@ -11,13 +11,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["signin"])) {
     // Call the loginUser function from Phpfunctions.php
     $loginResult = loginUser($conn, $email, $password);
     if ($loginResult) {
-        echo '<script>window.location.href = "index.php";</script>'; // Redirect to index.php if login is successful
+        // After successful login
+        $userId = getUserId($conn, $email); // Assuming you have a function to get userId by email
+        $_SESSION['userId'] = $userId; // Store userId in the session
+
+        // Redirect based on user type
+        $userType = getUserType($conn, $email);
+        if ($userType === 'Seller') {
+            echo '<script>window.location.href = "addProduct.html";</script>'; // Redirect to addProduct.html if user type is Seller
+        } else {
+            echo '<script>window.location.href = "index.php";</script>'; // Redirect to index.php for other user types
+        }
         exit();
     } else {
         // Alert message for incorrect login credentials
         echo '<script>alert("Login failed. Invalid email or password!");
-        window.location.href = "signin.html"
+        window.location.href = "signin.html";
         </script>';
     }
 }
+
 ?>
